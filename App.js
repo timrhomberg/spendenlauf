183e79c4f6204fc8afbbb6ae46aedc918777006d
rhombtim <rhombtim@students.zhaw.ch>
2021-10-05 14:56:37 +0200

    change app.js from function to class, add nfc package

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-    return (
-        <NavigationContainer style={styles.container}>
-            <Stack.Navigator>
-                <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-                <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+export default class App extends Component {
+
+    render() {
+        return (
+            <NavigationContainer style={styles.container}>
+                <Stack.Navigator>
+                    <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen}/>
+                    <Stack.Screen name="Home" component={HomeScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spendenlauf
+https://www.youtube.com/watch?v=ql4J6SpLXZA&ab_channel=MadeWithMatt
+https://www.youtube.com/watch?v=-2Z6PgJIGuM&ab_channel=DopeProgramming
